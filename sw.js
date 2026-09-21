c19971df60c07688d605e633e6459554630225e5
Cesar Natal Rodrigues <cesar.rodrigues@rnutrimax.com.br>
2026-09-21 14:58:27 -0300

    fix(multi-unidade): corrige ensureUnits (Map) e remove aba Geral; garante 4 unidades com todos os setores MOI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE = 'sop-capacidade-v22-multi';
+﻿const CACHE = 'sop-capacidade-v22-multi-2';
 const ASSETS = ['index.html', 'manifest.json', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/apple-touch-icon.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
